5b00356629e6bf0b575b0c164dcf57ead0947035
ranja <ranja@DESKTOP-UCMITSF>
2017-07-02 13:54:10 +0530

    02/07/2017

---
+++ seleniumProject/functions.py
@@ -0,0 +1,14 @@
+
+
+def myfunction():
+    pass
+
+sum(10,40)
+
+def sum(nstart,nend):
+    result = 0
+    for i in range(nstart,nend + 1):
+        result += 1
+    print(result)
+        
+    
--- seleniumProject/ifElse.py
+++ seleniumProject/ifElse.py
@@ -1,5 +1,31 @@
+# if else statement
+
 i=11
 if i % 2 == 0:
     print(str(i) + " is divisible by 2")
 else:
     print(str(i) + "is not divisible by 2 ")
+
+
+# nested is else Statement
+today =input("Enter the date")
+if today == "Monday":
+    print("Monday")
+elif today == "Tuesday":
+    print("Tesday")
+elif today == "Wednessday":
+    print("wednessday")
+else:
+    print("print not found in the list ")
+
+# nested Loop
+today = 'holiday'
+bankbalance = 2500
+if today == "holiday":
+    if bankbalance > 2500:
+        print("Go shopping")
+    else:
+        print("stay home")
+    
+else:
+    print('go to office')
